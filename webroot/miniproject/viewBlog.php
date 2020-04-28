2486bf0d44408f70689c0a909847b8789addcf0a
RohanDhother <rohandhother@MSI.localdomain>
2020-04-28 20:00:12 +0100

    changed

--- webroot/miniproject/viewBlog.php
+++ webroot/miniproject/viewBlog.php
@@ -4,10 +4,31 @@ $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("DATABASE_USER");
 $dbpwd = getenv("DATABASE_PASSWORD");
 $dbname = getenv("DATABASE_NAME");
+$Userlists = array();
 // Creates connection
 $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 // Checks connection
 if ($conn->connect_error) {
  die("Connection failed: " . $conn->connect_error);
+ $sql = "SELECT title, text, dateandtime FROM POSTS";
+ $result = $conn->query($sql);
+ if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $Users = array();
+        $Users[] = $row["id"];
+        $Users[] = $row["title"];
+        $Users[] = $row["text"];
+        $Users[] = $row["dateandtime"];
+    }
+if ($conn->query($sql) === TRUE) {
+echo "<h6>Registration Successful</h6>
+<p><a href='main.html'>Home</a></p>
+";
+} else {
+echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+}
 }
 ?>
