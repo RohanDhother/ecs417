636936abdaff23615a9442c371f34a326d5a9800
RohanDhother <rohandhother@MSI.localdomain>
2020-04-29 18:38:53 +0100

    changed

--- webroot/miniproject/login.php
+++ webroot/miniproject/login.php
@@ -20,12 +20,12 @@
   else {
     $sql = "SELECT password FROM USER WHERE username=$Username)";
     $password = $conn->query($sql);
-    echo "<p>password: " . $password->fetch_assoc();
+    echo "<p>password: " . $password;
     echo "<p>test";
     if($password->num_rows > 0)
     {
       echo "<p>test1";
-      if ($Password==$password->fetch_assoc())
+      if ($Password==$password) 
       {
         session_start();
         echo "<p>You are logged";
