e9943ddb27ad2d954f2a59e81e383299ebe83c60
RohanDhother <rohandhother@MSI.localdomain>
2020-04-29 19:23:46 +0100

    changed

--- webroot/miniproject/login.php
+++ webroot/miniproject/login.php
@@ -1,15 +1,11 @@
 <?php
   $Username = $_POST["username"];
   $Password = $_POST["password"];
-  $test = "test";
   $dbhost = getenv("MYSQL_SERVICE_HOST");
   $dbport = getenv("MYSQL_SERVICE_PORT");
   $dbuser = getenv("DATABASE_USER");
   $dbpwd = getenv("DATABASE_PASSWORD");
   $dbname = getenv("DATABASE_NAME");
-  echo "<p>Username: " . $Username;
-  echo "<p>Password: " . $Password;
-  echo "<p>test: " . $test;
   // Creates connection
   $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
   // Checks connection
@@ -19,7 +15,6 @@
   else {
     $sql = "SELECT password FROM USERS WHERE username='$Username'";
     $result = $conn->query($sql);
-    echo "<p> $result->num_rows: " . $result->num_rows;
     $result = $result->fetch_assoc();
     $password = $result["password"];
     if($password)
@@ -27,7 +22,6 @@
       if ($Password==$password)
       {
         session_start();
-        echo "<p>You are logged";
         include 'AddPost.html';
       }
       else {
