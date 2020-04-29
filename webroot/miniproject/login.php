5e2c14e6ff393f3b2408cdeb85ca06eb547f09f7
RohanDhother <rohandhother@MSI.localdomain>
2020-04-29 14:45:41 +0100

    changed

--- webroot/miniproject/login.php
+++ webroot/miniproject/login.php
@@ -2,7 +2,7 @@
   $Username = $POST["username"];
   $Password = $POST["password"];
   $sql = "SELECT password FROM USER WHERE username=$Username)";
-  $password = $result->fetch_assoc()]
+  $password = $result->fetch_assoc();
   if ($Password==$password)
   {
     session_start();
