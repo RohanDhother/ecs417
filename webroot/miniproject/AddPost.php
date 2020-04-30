af1a408fa2a1c412148ea5e3be381049ad634fe9
RohanDhother <rohandhother@MSI.localdomain>
2020-04-30 17:10:40 +0100

    changed

--- webroot/miniproject/AddPost.php
+++ webroot/miniproject/AddPost.php
@@ -4,10 +4,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
   $Content = $_POST["content"];
   $Date = date("d/m/Y");
   $Time = date("H:i");
-  echo "title: " . $Title;
-  echo "content: " . $Content;
-  echo "date: " . $Date;
-  echo "time: " . $Time;
+  echo "<p>title: " . $Title;
+  echo "<p>content: " . $Content;
+  echo "<p>date: " . $Date;
+  echo "<p>time: " . $Time;
   $dbhost = getenv("MYSQL_SERVICE_HOST");
   $dbport = getenv("MYSQL_SERVICE_PORT");
   $dbuser = getenv("DATABASE_USER");
@@ -20,9 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     die("Connection failed: " . $conn->connect_error);
   }
   else {
-
+    echo "<p>test";
     $sql = "INSERT INTO Posts (title, content, date_set, time_set) VALUES ('$Title', '$Content','$Date', '$Time')";
-    echo "test";
+    echo "<p>test2";
   }
   $conn->close();
 }
