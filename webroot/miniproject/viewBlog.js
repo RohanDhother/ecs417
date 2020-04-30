961bef7ad0c9d45a27104a993cf563916f6d012e
RohanDhother <rohandhother@MSI.localdomain>
2020-04-30 13:31:30 +0100

    changed

--- webroot/miniproject/viewBlog.js
+++ webroot/miniproject/viewBlog.js
@@ -1,26 +1,28 @@
-// var Postlists = <?php getPosts();?>;
-var posts = document.getElementById('Posts');
-let i = 0;
-while(i<2)
+function LoadBlog(Postlists)
 {
-  let blogbox = document.createElement('div');
-  blogbox.className('Blog-box');
-  let dateandtime = document.createElement('div');
-  dateandtime.className('dateandtime');
-  let dateandtimeText = createTextNode("dateandtime");
-  dateandtime.appendChild(dateandtimeText);
-  blogbox.appendChild(dateandtime);
-  let title = document.createElement('div');
-  title.className('title');
-  let titleText = document.createTextNode("title");
-  title.appendChild(titleText);
-  blogbox.appendChild(title);
-  let content = document.createElement('div');
-  content.className('text');
-  let contentText = document.createTextNode("text");
-  content.appendChild(contentText);
-  blogbox.appendChild(content);
-  posts.appendChild(blogbox);
-  i++;
+  var posts = document.getElementById('Posts');
+        var i = 0;
+        while(i<2)
+        {
+          let blogbox = document.createElement('div');
+          blogbox.className = 'Blog-box';
+          let dateandtime = document.createElement('div');
+          dateandtime.className = 'dateandtime';
+          let dateandtimeText = document.createTextNode("dateandtime");
+          dateandtime.appendChild(dateandtimeText);
+          blogbox.appendChild(dateandtime);
+          let title = document.createElement('div');
+          title.className = 'title';
+          let titleText = document.createTextNode("title");
+          title.appendChild(titleText);
+          blogbox.appendChild(title);
+          let content = document.createElement('div');
+          content.className = 'text';
+          let contentText = document.createTextNode("text");
+          content.appendChild(contentText);
+          blogbox.appendChild(content);
+          posts.appendChild(blogbox);
+          i++;
+        }
+
 }
-// </script>
--- webroot/miniproject/viewBlog.php
+++ webroot/miniproject/viewBlog.php
@@ -34,30 +34,30 @@ function getPosts(){
 // echo "test";
 // echo '<script src="viewBlog.js"></script>';
 // echo "test2";
-echo "<script type="text/JavaScript">
-      var posts = document.getElementById('Posts');
-      var i = 0;
-      while(i<2)
-      {
-        let blogbox = document.createElement('div');
-        blogbox.className = 'Blog-box';
-        let dateandtime = document.createElement('div');
-        dateandtime.className = 'dateandtime';
-        let dateandtimeText = document.createTextNode("dateandtime");
-        dateandtime.appendChild(dateandtimeText);
-        blogbox.appendChild(dateandtime);
-        let title = document.createElement('div');
-        title.className = 'title';
-        let titleText = document.createTextNode("title");
-        title.appendChild(titleText);
-        blogbox.appendChild(title);
-        let content = document.createElement('div');
-        content.className = 'text';
-        let contentText = document.createTextNode("text");
-        content.appendChild(contentText);
-        blogbox.appendChild(content);
-        posts.appendChild(blogbox);
-        i++;
-      }
-      </script>";
+echo "<script src="viewBlog.js" type="text/JavaScript">var postlists = $Postlists; LoadBlog(postlists);</script>";
+      // var posts = document.getElementById('Posts');
+      // var i = 0;
+      // while(i<2)
+      // {
+      //   let blogbox = document.createElement('div');
+      //   blogbox.className = 'Blog-box';
+      //   let dateandtime = document.createElement('div');
+      //   dateandtime.className = 'dateandtime';
+      //   let dateandtimeText = document.createTextNode("dateandtime");
+      //   dateandtime.appendChild(dateandtimeText);
+      //   blogbox.appendChild(dateandtime);
+      //   let title = document.createElement('div');
+      //   title.className = 'title';
+      //   let titleText = document.createTextNode("title");
+      //   title.appendChild(titleText);
+      //   blogbox.appendChild(title);
+      //   let content = document.createElement('div');
+      //   content.className = 'text';
+      //   let contentText = document.createTextNode("text");
+      //   content.appendChild(contentText);
+      //   blogbox.appendChild(content);
+      //   posts.appendChild(blogbox);
+      //   i++;
+      // }
+      // </script>";
 ?>
