74262f28ce6a6abad65357bc8cceb0d3a9febd48
RohanDhother <rohandhother@MSI.localdomain>
2020-05-01 19:18:26 +0100

    changed

--- webroot/miniproject/logout.php
+++ webroot/miniproject/logout.php
@@ -1,11 +1,11 @@
 <?php
 // remove all session variables
 try{
-  session_unset();
-
-  // destroy the session
+  // session_unset();
+  //
+  // // destroy the session
   session_destroy();
-  return include "index.php";
+  include "index.php";
 }
 catch (Exception $e){}
  ?>
