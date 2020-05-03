21b7841444660ff686391dc3111dc5a42f404374
RohanDhother <rohandhother@MSI.localdomain>
2020-05-03 21:13:16 +0100

    changed

--- webroot/miniproject/addpost.js
+++ webroot/miniproject/addpost.js
@@ -14,7 +14,7 @@ function validateaddpost()
   var text = document.forms["addpost"]["text"].value;
   if(title=='' && text=='')
   {
-    alert("Both");
+    alert("Please enter all field");
     document.getElementById('title').style.backgroundColor ="red";
     document.getElementById('text').style.backgroundColor ="red";
     document.getElementById('title').style.color ="white";
@@ -23,12 +23,16 @@ function validateaddpost()
   }
   else if(title=='')
   {
-    alert("title");
+    alert("Please enter the title field");
+    document.getElementById('title').style.backgroundColor ="red";
+    document.getElementById('title').style.color ="white";
     return false;
   }
   else if(text=='')
   {
-    alert("text");
+    alert("Please enter the content field");
+    document.getElementById('text').style.backgroundColor ="red";
+    document.getElementById('text').style.color ="white";
     return false;
   }
 }
