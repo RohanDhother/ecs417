833c9f2f4ab01f43c2a4c21f8bc69beac53c75de
RohanDhother <rohandhother@MSI.localdomain>
2020-05-03 15:09:41 +0100

    changed

--- webroot/miniproject/AddPost.php
+++ webroot/miniproject/AddPost.php
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $result = $conn->query($sql);
     $result = $result->fetch_assoc();
     echo "<p>result: " . $result;
-    $DateandTime = $result[0];
+    $DateandTime = $result[];
     echo "<p>dateandtime: " . $DateandTime;
     $sql = "INSERT INTO POSTS (title, content, dateandtime)VALUES ($Title, $Content, $DateandTime)";
     echo "<p>sql: " . $sql;
