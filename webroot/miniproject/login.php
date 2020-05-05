4f10d2175a98118053b7ebe385ba9f1cadbb2ecb
RohanDhother <rohandhother@MSI.localdomain>
2020-05-05 20:03:16 +0100

    changed

--- webroot/miniproject/login.php
+++ webroot/miniproject/login.php
@@ -8,13 +8,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
   $dbuser = getenv("DATABASE_USER");
   $dbpwd = getenv("DATABASE_PASSWORD");
   $dbname = getenv("DATABASE_NAME");
-  // Creates connection
   $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-  // Checks connection
   if ($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
-   $conn->close();
-  }
+   $conn->close();}
   else {
     $sql = "SELECT password FROM USERS WHERE email='$Email'";
     $result = $conn->query($sql);
@@ -27,29 +24,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         session_start();
         $_SESSION["email"] = $Email;
         $conn->close();
-        // return include 'AddPost.html';
         header('Location: http://cakephp-mysql-persistent-rohandhotherecs417.bde1.qmul-eecs.openshiftapps.com/miniproject/AddPost.html');
       }
       else {
         $conn->close();
         include 'index.php';
         echo '<script type="text/JavaScript">',
-              'alert("Email wrong");',
+              'alert("Password wrong");',
               '</script>'
               ;
-        // header('Location: http://cakephp-mysql-persistent-rohandhotherecs417.bde1.qmul-eecs.openshiftapps.com/miniproject/index.php');
       }
     }
     else {
       $conn->close();
       include 'index.php';
-      //header('Location: http://cakephp-mysql-persistent-rohandhotherecs417.bde1.qmul-eecs.openshiftapps.com/miniproject/index.php');
       echo '<script type="text/JavaScript">',
-            'alert("Password Wrong");',
+            'alert("Email Wrong");',
             '</script>'
             ;
-      //header('Location: http://cakephp-mysql-persistent-rohandhotherecs417.bde1.qmul-eecs.openshiftapps.com/miniproject/index.php');
-      // header('Location: http://cakephp-mysql-persistent-rohandhotherecs417.bde1.qmul-eecs.openshiftapps.com/miniproject/index.php');
     }
 }
 }
