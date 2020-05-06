0c08e0e5b56bcce6b9cedb620c24d0102953634e
RohanDhother <rohandhother@MSI.localdomain>
2020-05-06 14:11:27 +0100

    changed

--- webroot/miniproject/index.php
+++ webroot/miniproject/index.php
@@ -1,5 +1,5 @@
 <?php
   session_start();
-include "http://cakephp-mysql-persistent-rohandhotherecs417.bde1.qmul-eecs.openshiftapps.com/miniproject/html/main.html";
-include "http://cakephp-mysql-persistent-rohandhotherecs417.bde1.qmul-eecs.openshiftapps.com/miniproject/php/viewBlog.php";
+include "/html/main.html";
+include "/php/viewBlog.php";
 ?>
